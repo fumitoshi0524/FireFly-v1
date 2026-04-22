a1477e87d505582c82930d0ba82d06dfef74ef0c
fumitoshi0524 <Fufumitoshi@qq.com>
2026-04-23 00:16:22 +0800

    Fix init the module which doesn't exist

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,21 +1,15 @@
 from train_util import (
     get_lr,
     Logger,
-    is_main_process,
     lm_checkpoint,
-    init_distributed_mode,
     setup_seed,
     init_model,
-    SkipBatchSampler,
 )
 
 __all__ = [
     "get_lr",
     "Logger",
-    "is_main_process",
     "lm_checkpoint",
-    "init_distributed_mode",
     "setup_seed",
     "init_model",
-    "SkipBatchSampler",
 ]
